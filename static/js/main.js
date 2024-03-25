24061b569efb9a5b305039bdaea6b29109d687e2
Timofey Basuev <echiauta@gmail.com>
2024-03-25 23:07:11 +0500

    update something

---
+++ static/js/main.js
@@ -0,0 +1,26 @@
+function zoomIn(el) {
+    el.style.transform = "scale(1.5)";
+}
+function zoomOut(el) {
+    el.style.transform = "";
+}
+function cycleImages() {
+    const imgPath = "./static/img/"
+    const img = document.getElementById("img");
+
+    const photosPathes = ["img-4.jpg", "me-1.jpg", "img-2.jpg", "img-3.jpg", "me-3.jpg", "img-5.jpg"];
+    const photosQuantity = photosPathes.length;
+
+    let counter = 1;
+    let interval = setInterval(function() {
+        img.src = imgPath + photosPathes[counter];
+
+        counter++;
+        if (counter == photosQuantity) {
+            counter = 0;
+        }
+    }, 5000);
+}
+window.onload = function() {
+    cycleImages();
+}
